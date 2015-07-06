e8a1d67bb859a17f1f1530795a8c69c8f64ced45
juanpablogdl <juanpablo@MBP-de-Juan.lan>
2015-07-05 22:55:42 -0500

    new functionality
    new functionality

---
+++ templates/page-home-slider.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Front Page
+ *
+ * @package Dinamarca
+ */
+
+get_header(); ?>
+
+<?php // Load Slider ?>
+<?php $titan = TitanFramework::getInstance( 'dinamarca' ); ?>
+<?php if ($titan->getOption( 'show_slider' )) { ?>
+        <?php get_template_part( 'template-parts/index', 'slider' ); ?>
+<?php } ?>
+
+
+<div id="primary" class="content-area no-sidebar">
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/page', 'index' ); ?>
+
+		<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+
+<?php // Load Testimonials ?>
+<?php $titan = TitanFramework::getInstance( 'dinamarca' ); ?>
+<?php if ($titan->getOption( 'show_testimonials' )) { ?>
+    <?php get_template_part( 'template-parts/index', 'testimonials' ); ?>
+<?php } ?>
+
+
+
+<?php get_footer(); ?>
